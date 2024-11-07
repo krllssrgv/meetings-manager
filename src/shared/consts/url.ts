359b14e9f2d151c116e17f0f8d712aca101051d5
Kirill Savin <krllssrgv@gmail.com>
2024-11-07 12:47:42 +0300

    New API URL

--- src/shared/consts/url.ts
+++ src/shared/consts/url.ts
@@ -1,1 +1,2 @@
-export const API_URL = 'http://127.0.0.1:5000/api/';
+// export const API_URL = 'http://127.0.0.1:5000/api/';
+export const API_URL = 'https://krllssrgv-cm-server-08a8.twc1.net/api/';
